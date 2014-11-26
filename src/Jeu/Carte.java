694ee8e6fdcb0fe90e5c8724b17ea0b4fe6352b2
Aurelien.Wolz <wolzaurelien@gmail.com>
2014-11-26 23:27:58 +0100

    Ajout méthode pour push des images sur Github --> Description dans Carte.java en commentaire

--- src/Jeu/Carte.java
+++ src/Jeu/Carte.java
@@ -3,6 +3,14 @@ package Jeu;
 /**
  * Created by ravier on 15/11/14.
  */
+
+/**
+ * Methode pour push des images sur Github :
+ * Se déplacer dans le dossier des images à push
+ * > git add .
+ * > git commit -m <Message>
+ * > git push -u origin master
+ */
 public class Carte {
     private String nom;
 
